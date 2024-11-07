aa5b88104f7acb9679eaed1ccf42e7f87dc71021
gelubswag <doshdik35@gmail.com>
2024-11-08 00:22:06 +0300

    Delete spravApp directory

--- spravApp/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Name, Fam, Otc, Street, MainItem, Phones
-
-for i in [Name, Fam, Otc, Street, MainItem, Phones]:
-    admin.site.register(i)
-# Register your models here.
--- spravApp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class SpravappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'spravApp'
